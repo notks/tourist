fb429f7ab10d58224a729e7e9b7f4aa5a4850bee
notks <kemal.selimbasic.2016@gmail.com>
2021-10-31 13:14:07 +0100

    Add page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/pages/landing-page/landing-page.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { AdminComponent } from './components/pages/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
